3bdfc933c0a73c006f9f697df8f9dba9e2027adc
omarFaruk <2035faruk@gmail.com>
2025-05-14 11:45:26 +0600

    add 'task management' button in under admin dashboard and use 'tasks' route

--- src/layout/AppMenu.tsx
+++ src/layout/AppMenu.tsx
@@ -1,22 +1,33 @@
-import type {MenuModel} from "../../types";
+import type { MenuModel } from "../../types";
+import { useAuth } from "../GlobalProvider/useData/AuthContext";
 import AppSubMenu from "./AppSubMenu";
 
 const AppMenu = () => {
-    const model: MenuModel[] = [
+  const { isAdmin } = useAuth();
+  const model: MenuModel[] = [
+    {
+      label: "Dashboards",
+      icon: "pi pi-home",
+      items: [
         {
-            label: "Dashboards",
-            icon: "pi pi-home",
-            items: [
-                {
-                    label: "Home",
-                    icon: "pi pi-fw pi-home",
-                    to: "/",
-                },
-            ],
+          label: "Home",
+          icon: "pi pi-fw pi-home",
+          to: "/",
         },
-    ];
+        ...(isAdmin()
+          ? [
+              {
+                label: "Task Management",
+                icon: "pi pi-fw pi-list",
+                to: "/tasks",
+              },
+            ]
+          : []),
+      ],
+    },
+  ];
 
-    return <AppSubMenu model={model}/>;
+  return <AppSubMenu model={model} />;
 };
 
 export default AppMenu;
